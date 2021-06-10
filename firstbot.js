b292009369595b7e625f8962deede8d10ef80a7e
Victoria Crouthamel <victoriacrouthamel@gmail.com>
2021-06-10 12:54:38 -0400

    Uploading bot project

---
+++ firstbot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log ('Nowbot is online!');
+});
+
+//Keep at last line of file
+client.login('ODUxODU5NDgwODY5ODYzNDk1.YL-Z9w.GyDNdRAUZSPbmJ_EWgjIj80bsnE');
